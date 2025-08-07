0824c820a298c9c324363a8bedfdba8ceeca4bba
Mark Kenneth Bayona <mkmbayona@gmail.com>
2025-08-07 22:29:00 +0800

    Adds basic tests for a RemoteCatFactLoader

---
+++ MeowMeowMeowMeowTests/RemoteCatFactLoaderTests.swift
@@ -0,0 +1,60 @@
+//
+//  RemoteCatFactLoaderTests.swift
+//  MeowMeowMeowMeow
+//
+//  Created by Mark Kenneth Bayona on 8/7/25.
+//
+
+import XCTest
+
+class RemoteCatFactLoader {
+    let url: URL
+    let client: HTTPClient
+
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+
+    func load() {
+        client.get(from: url)
+    }
+}
+
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+final class RemoteCatFactLoaderTests: XCTestCase {
+
+    func test_init_doesNotRequestDataFromURL() {
+        let (_, client) = makeSUT()
+
+        XCTAssertNil(client.requestedURL)
+    }
+
+    func test_load_requestDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+
+        sut.load()
+
+        XCTAssertEqual(client.requestedURL, url)
+    }
+
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+
+        func get(from url: URL) {
+            requestedURL = url
+        }
+    }
+
+    private func makeSUT(
+        url: URL = URL(string: "https://a-url.com")!
+    ) -> (sut: RemoteCatFactLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let remoteCatFactLoader = RemoteCatFactLoader(url: url, client: client)
+        return (sut: remoteCatFactLoader, client: client)
+    }
+}
