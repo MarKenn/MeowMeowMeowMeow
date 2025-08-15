279831aa553b7083f1f40b62eac550721db670bc
Mark Kenneth Bayona <mkmbayona@gmail.com>
2025-08-15 11:21:49 +0800

    Replace magic number with variable giving extra context

--- MeowMeowMeowMeow/CatFactAPI/RemoteCatFactLoader.swift
+++ MeowMeowMeowMeow/CatFactAPI/RemoteCatFactLoader.swift
@@ -57,8 +57,10 @@ private struct CatFactsMapper {
         let data: [String]
     }
 
+    static var OK_200: Int { return 200 }
+
     static func map(_ data: Data, response: HTTPURLResponse) throws -> [String] {
-        guard response.statusCode == 200 else {
+        guard response.statusCode == OK_200 else {
             throw RemoteCatFactLoader.Error.invalidData
         }
 
