8e0cc6c43f88e0334d18619f009a3df315ee8083
Mark Kenneth Bayona <mkmbayona@gmail.com>
2025-08-21 16:27:51 +0800

    Make URLSessionHTTPClient conform to the HTTPClient protocol. Also move URLSessionHTTPClient to its own file in production.

--- MeowMeowMeowMeowTests/CatFactAPI/URLSessionHTTPClientTests.swift
+++ MeowMeowMeowMeowTests/CatFactAPI/URLSessionHTTPClientTests.swift
@@ -8,28 +8,6 @@
 import XCTest
 import MeowMeowMeowMeow
 
-class URLSessionHTTPClient {
-    let session: URLSession
-
-    init(session: URLSession = .shared) {
-        self.session = session
-    }
-
-    struct UnexpectedValueRepresentation: Error {}
-
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-        session.dataTask(with: url) { data, response, error in
-            if let error {
-                completion(.failure(error))
-            } else if let data, let response = response as? HTTPURLResponse {
-                completion(.success((data, response)))
-            }else {
-                completion(.failure(UnexpectedValueRepresentation()))
-            }
-        }.resume()
-    }
-}
-
 final class URLSessionHTTPClientTests: XCTestCase {
 
     override func setUp() {
@@ -100,7 +78,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
 
     // MARK: - Helpers
 
-    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> URLSessionHTTPClient {
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> HTTPClient {
         let sut = URLSessionHTTPClient()
         trackFOrMemoryLeaks(sut, file: file, line: line)
         return sut
