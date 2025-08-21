bd0c9bc57c09aa1e7aae4c848e05c05cce4f19e1
Mark Kenneth Bayona <mkmbayona@gmail.com>
2025-08-21 09:55:51 +0800

    Move start/stop intercepting requests to setup/tearDown methods

--- MeowMeowMeowMeowTests/CatFactAPI/URLSessionHTTPClientTests.swift
+++ MeowMeowMeowMeowTests/CatFactAPI/URLSessionHTTPClientTests.swift
@@ -26,8 +26,17 @@ class URLSessionHTTPClient {
 
 final class URLSessionHTTPClientTests: XCTestCase {
 
-    func test_getFromURL_performsGETRequestWithURL() {
+    override func setUp() {
+        super.setUp()
         URLProtocolStub.startInterceptingRequests()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        URLProtocolStub.stopInterceptingRequests()
+    }
+
+    func test_getFromURL_performsGETRequestWithURL() {
         let url = URL(string: "https://any-url.com")!
         let exp = expectation(description: "Wait for request")
 
@@ -40,11 +49,9 @@ final class URLSessionHTTPClientTests: XCTestCase {
         URLSessionHTTPClient().get(from: url) { _ in }
 
         wait(for: [exp], timeout: 1.0)
-        URLProtocolStub.stopInterceptingRequests()
     }
 
     func test_getFromURL_failsOnRequestError() {
-        URLProtocolStub.startInterceptingRequests()
         let url = URL(string: "https://a-url.com")!
         let expectedError = NSError(domain: "any error", code: 1)
         let sut = URLSessionHTTPClient()
@@ -65,7 +72,6 @@ final class URLSessionHTTPClientTests: XCTestCase {
         }
 
         wait(for: [exp], timeout: 1.0)
-        URLProtocolStub.startInterceptingRequests()
     }
 
     // MARK: - Helpers
