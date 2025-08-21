bde71e429964f4d06d5192e1a6f7496fe4bdeed2
Mark Kenneth Bayona <mkmbayona@gmail.com>
2025-08-21 10:29:49 +0800

    Move URL creation to a shared factory method

--- MeowMeowMeowMeowTests/CatFactAPI/URLSessionHTTPClientTests.swift
+++ MeowMeowMeowMeowTests/CatFactAPI/URLSessionHTTPClientTests.swift
@@ -37,7 +37,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
     }
 
     func test_getFromURL_performsGETRequestWithURL() {
-        let url = URL(string: "https://any-url.com")!
+        let url = anyURL()
         let exp = expectation(description: "Wait for request")
 
         URLProtocolStub.observeRequest { request in
@@ -52,7 +52,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
     }
 
     func test_getFromURL_failsOnRequestError() {
-        let url = URL(string: "https://a-url.com")!
+        let url = anyURL()
         let expectedError = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: expectedError)
 
@@ -79,6 +79,10 @@ final class URLSessionHTTPClientTests: XCTestCase {
         let sut = URLSessionHTTPClient()
         trackFOrMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+
+    private func anyURL() -> URL {
+        URL(string: "https://any-url.com")!
     }
 
     private class URLProtocolStub: URLProtocol {
