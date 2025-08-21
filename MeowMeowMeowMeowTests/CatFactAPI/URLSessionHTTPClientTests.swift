5d4afe749c03b0f3538c7702a08d2579ddcf965b
Mark Kenneth Bayona <mkmbayona@gmail.com>
2025-08-21 09:49:38 +0800

    Guarantees correct URL and HTTP method on get requests

--- MeowMeowMeowMeowTests/CatFactAPI/URLSessionHTTPClientTests.swift
+++ MeowMeowMeowMeowTests/CatFactAPI/URLSessionHTTPClientTests.swift
@@ -26,6 +26,23 @@ class URLSessionHTTPClient {
 
 final class URLSessionHTTPClientTests: XCTestCase {
 
+    func test_getFromURL_performsGETRequestWithURL() {
+        URLProtocolStub.startInterceptingRequests()
+        let url = URL(string: "https://any-url.com")!
+        let exp = expectation(description: "Wait for request")
+
+        URLProtocolStub.observeRequest { request in
+            XCTAssertEqual(request.url, url)
+            XCTAssertEqual(request.httpMethod, "GET")
+            exp.fulfill()
+        }
+
+        URLSessionHTTPClient().get(from: url) { _ in }
+
+        wait(for: [exp], timeout: 1.0)
+        URLProtocolStub.stopInterceptingRequests()
+    }
+
     func test_getFromURL_failsOnRequestError() {
         URLProtocolStub.startInterceptingRequests()
         let url = URL(string: "https://a-url.com")!
@@ -54,7 +71,8 @@ final class URLSessionHTTPClientTests: XCTestCase {
     // MARK: - Helpers
 
     class URLProtocolStub: URLProtocol {
-        static var stub: Stub?
+        private static var stub: Stub?
+        private static var requestObserver: ((URLRequest) -> Void)?
 
         struct Stub {
             let data: Data?
@@ -66,6 +84,10 @@ final class URLSessionHTTPClientTests: XCTestCase {
             stub = Stub(data: data, response: response, error: error)
         }
 
+        static func observeRequest(_ observer: @escaping (URLRequest) -> Void) {
+            requestObserver = observer
+        }
+
         static func startInterceptingRequests() {
             URLProtocol.registerClass(URLProtocolStub.self)
         }
@@ -73,9 +95,11 @@ final class URLSessionHTTPClientTests: XCTestCase {
         static func stopInterceptingRequests() {
             URLProtocol.unregisterClass(URLProtocolStub.self)
             stub = nil
+            requestObserver = nil
         }
 
         override class func canInit(with request: URLRequest) -> Bool {
+            requestObserver?(request)
             return true
         }
 
