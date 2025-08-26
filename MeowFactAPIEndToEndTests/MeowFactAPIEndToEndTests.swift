c924870ea99931b883a9bf62ed15bbb6fa6f8fa0
Mark Kenneth Bayona <mkmbayona@gmail.com>
2025-08-26 16:02:26 +0800

    Extract test setup to a helper method

--- MeowFactAPIEndToEndTests/MeowFactAPIEndToEndTests.swift
+++ MeowFactAPIEndToEndTests/MeowFactAPIEndToEndTests.swift
@@ -11,6 +11,20 @@ import MeowMeowMeowMeow
 final class MeowFactAPIEndToEndTests: XCTestCase {
 
     func test_endToEndServerGETCatFact_isNotEmpty() {
+        switch getCatFact() {
+        case .success(let catFact):
+            let trimmedFact = catFact.trimmingCharacters(in: .whitespacesAndNewlines)
+            XCTAssertFalse(trimmedFact.isEmpty)
+        case .failure(let error):
+            XCTFail("Expecting success, got \(error) instead")
+        case .none:
+            XCTFail("Expecting success, got no instead")
+        }
+    }
+
+    // MARK: - Helpers
+
+    func getCatFact() -> RemoteCatFactLoader.Result? {
         let url = URL(string: "https://meowfacts.herokuapp.com/")!
         let client = URLSessionHTTPClient()
         let loader = RemoteCatFactLoader(url: url, client: client)
@@ -25,14 +39,7 @@ final class MeowFactAPIEndToEndTests: XCTestCase {
         }
 
         wait(for: [exp], timeout: 5.0)
-
-        switch receivedResult {
-        case .success(let catFact):
-            let trimmedFact = catFact.trimmingCharacters(in: .whitespacesAndNewlines)
-            XCTAssertFalse(trimmedFact.isEmpty)
-        default:
-            XCTFail("Expecting success, got \(String(describing: receivedResult)) instead")
-        }
+        return receivedResult
     }
 
 }
