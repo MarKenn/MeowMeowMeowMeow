cb48cabba54fe5527a1441e11564e2aa934a9c30
Mark Kenneth Bayona <mkmbayona@gmail.com>
2025-09-25 21:25:56 +0800

    Save command request cache insertion with timestamp on successful deletion

--- MeowMeowMeowMeowTests/CatFactCache/CacheCatFactUseCaseTests.swift
+++ MeowMeowMeowMeowTests/CatFactCache/CacheCatFactUseCaseTests.swift
@@ -8,16 +8,18 @@
 import XCTest
 
 class LocalCatFactLoader {
-  let store: CatFactStore
+  private let store: CatFactStore
+  private let currentDate: () -> Date
 
-  init(store: CatFactStore) {
+  init(store: CatFactStore, currentDate: @escaping () -> Date) {
     self.store = store
+    self.currentDate = currentDate
   }
 
   func save(_ facts: [String]) {
     store.deleteCachedFacts { [unowned self] error in
       if error == nil {
-        store.insert(facts)
+        store.insert(facts, timestamp: currentDate())
       }
     }
   }
@@ -28,8 +30,9 @@ class CatFactStore {
 
   var deleteCachedFactsCount = 0
   var insertCallCount = 0
+  var insertions = [(facts: [String], timestamp: Date)]()
 
-  private var deletionCompletions: [DeleteCompletion] = []
+  private var deletionCompletions = [DeleteCompletion]()
 
   func deleteCachedFacts(completion: @escaping DeleteCompletion) {
     deleteCachedFactsCount += 1
@@ -44,8 +47,9 @@ class CatFactStore {
     deletionCompletions[index](nil)
   }
 
-  func insert(_ facts: [String]) {
+  func insert(_ facts: [String], timestamp: Date) {
     insertCallCount += 1
+    insertions.append((facts, timestamp))
   }
 }
 
@@ -87,11 +91,28 @@ final class CacheCatFactUseCaseTests: XCTestCase {
     XCTAssertEqual(store.insertCallCount, 1)
   }
 
+  func test_save_requestsNewCacheInsertionWithTimestampOnSuccessfulDeletion() {
+    let currentDate = Date()
+    let facts: [String] = [anyFact(), anyFact()]
+    let (sut, store) = makeSUT(currentDate: { currentDate })
+
+    sut.save(facts)
+    store.completeDeletionSuccessfully()
+
+    XCTAssertEqual(store.insertions.count, 1)
+    XCTAssertEqual(store.insertions.first?.facts, facts)
+    XCTAssertEqual(store.insertions.first?.timestamp, currentDate)
+  }
+
   // MARK: Helpers
 
-  private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (LocalCatFactLoader, CatFactStore) {
+  private func makeSUT(
+    currentDate: @escaping () -> Date = Date.init,
+    file: StaticString = #filePath,
+    line: UInt = #line
+  ) -> (LocalCatFactLoader, CatFactStore) {
     let store = CatFactStore()
-    let sut = LocalCatFactLoader(store: store)
+    let sut = LocalCatFactLoader(store: store, currentDate: currentDate)
     trackForMemoryLeaks(store, file: file, line: line)
     trackForMemoryLeaks(sut, file: file, line: line)
     return (sut, store)
