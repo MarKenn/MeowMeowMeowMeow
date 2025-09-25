d29617ec2c30ce6bc632fcf77f18db213ebd1573
Mark Kenneth Bayona <mkmbayona@gmail.com>
2025-09-25 22:07:21 +0800

    Save command succeeds on successful insertion

--- MeowMeowMeowMeowTests/CatFactCache/CacheCatFactUseCaseTests.swift
+++ MeowMeowMeowMeowTests/CatFactCache/CacheCatFactUseCaseTests.swift
@@ -62,6 +62,10 @@ class CatFactStore {
   func completeInsertion(with error: Error, at index: Int = 0) {
     insertionCompletions[index](error)
   }
+
+  func completeInsertionSuccessfully(at index: Int = 0) {
+    insertionCompletions[index](nil)
+  }
 }
 
 final class CacheCatFactUseCaseTests: XCTestCase {
@@ -138,6 +142,24 @@ final class CacheCatFactUseCaseTests: XCTestCase {
     wait(for: [exp], timeout: 1.0)
 
     XCTAssertEqual(receivedError as? NSError, insertionError)
+  }
+
+  func test_save_succeedsOnSuccessfulCacheInsertion() {
+    let facts: [String] = [anyFact(), anyFact()]
+    let (sut, store) = makeSUT()
+    let exp = expectation(description: "Wait for save completion")
+
+    var receivedError: Error?
+    sut.save(facts) { error in
+      receivedError = error
+      exp.fulfill()
+    }
+
+    store.completeDeletionSuccessfully()
+    store.completeInsertionSuccessfully()
+    wait(for: [exp], timeout: 1.0)
+
+    XCTAssertNil(receivedError)
   }
 
   // MARK: Helpers
