b4b14d3d5347eac0055548a730e57016b13d3bd2
Mark Kenneth Bayona <mkmbayona@gmail.com>
2025-09-26 08:12:38 +0800

    Re-indent to 4 spaces

--- MeowMeowMeowMeowTests/CatFactCache/CacheCatFactUseCaseTests.swift
+++ MeowMeowMeowMeowTests/CatFactCache/CacheCatFactUseCaseTests.swift
@@ -8,179 +8,179 @@
 import XCTest
 
 class LocalCatFactLoader {
-  private let store: CatFactStore
-  private let currentDate: () -> Date
-
-  init(store: CatFactStore, currentDate: @escaping () -> Date) {
-    self.store = store
-    self.currentDate = currentDate
-  }
-
-  func save(_ facts: [String], completion: @escaping (Error?) -> Void) {
-    store.deleteCachedFacts { [unowned self] error in
-      if error == nil {
-        store.insert(facts, timestamp: currentDate(), completion: completion)
-      } else {
-        completion(error)
-      }
+    private let store: CatFactStore
+    private let currentDate: () -> Date
+    
+    init(store: CatFactStore, currentDate: @escaping () -> Date) {
+        self.store = store
+        self.currentDate = currentDate
     }
-  }
+    
+    func save(_ facts: [String], completion: @escaping (Error?) -> Void) {
+        store.deleteCachedFacts { [unowned self] error in
+            if error == nil {
+                store.insert(facts, timestamp: currentDate(), completion: completion)
+            } else {
+                completion(error)
+            }
+        }
+    }
 }
 
 protocol CatFactStore {
-  typealias DeleteCompletion = (Error?) -> Void
-  typealias InsertCompletion = (Error?) -> Void
-
-  func deleteCachedFacts(completion: @escaping DeleteCompletion)
-  func insert(_ facts: [String], timestamp: Date, completion: @escaping InsertCompletion)
+    typealias DeleteCompletion = (Error?) -> Void
+    typealias InsertCompletion = (Error?) -> Void
+    
+    func deleteCachedFacts(completion: @escaping DeleteCompletion)
+    func insert(_ facts: [String], timestamp: Date, completion: @escaping InsertCompletion)
 }
 
 final class CacheCatFactUseCaseTests: XCTestCase {
-
-  func test_init_doesNotDeleteCacheUponCreation() {
-    let (_, store) = makeSUT()
-
-    XCTAssertEqual(store.receivedMessages, [])
-  }
-
-  func test_save_requestsCacheDeletion() {
-    let facts: [String] = [anyFact(), anyFact()]
-    let (sut, store) = makeSUT()
-
-    sut.save(facts) { _ in }
-
-    XCTAssertEqual(store.receivedMessages, [.deleteCachedFacts])
-  }
-
-  func test_save_doesNotRequestsCacheInsertionOnDeletionError() {
-    let facts: [String] = [anyFact(), anyFact()]
-    let (sut, store) = makeSUT()
-    let deletionError = anyNSError()
-
-    sut.save(facts) { _ in }
-    store.completeDeletion(with: deletionError)
-
-    XCTAssertEqual(store.receivedMessages, [.deleteCachedFacts])
-  }
-
-  func test_save_requestsNewCacheInsertionWithTimestampOnSuccessfulDeletion() {
-    let currentDate = Date()
-    let facts: [String] = [anyFact(), anyFact()]
-    let (sut, store) = makeSUT(currentDate: { currentDate })
-
-    sut.save(facts) { _ in }
-    store.completeDeletionSuccessfully()
-
-    XCTAssertEqual(store.receivedMessages, [.deleteCachedFacts, .insert(facts, currentDate)])
-  }
-
-  func test_save_failsOnDeletionError() {
-    let (sut, store) = makeSUT()
-    let deletionError = anyNSError()
-
-    expect(sut, toCompleteWith: deletionError) {
-      store.completeDeletion(with: deletionError)
+    
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
     }
-  }
-
-  func test_save_failsOnInsertionError() {
-    let (sut, store) = makeSUT()
-    let insertionError = anyNSError()
-
-    expect(sut, toCompleteWith: insertionError) {
-      store.completeDeletionSuccessfully()
-      store.completeInsertion(with: insertionError)
+    
+    func test_save_requestsCacheDeletion() {
+        let facts: [String] = [anyFact(), anyFact()]
+        let (sut, store) = makeSUT()
+        
+        sut.save(facts) { _ in }
+        
+        XCTAssertEqual(store.receivedMessages, [.deleteCachedFacts])
     }
-  }
-
-  func test_save_succeedsOnSuccessfulCacheInsertion() {
-    let (sut, store) = makeSUT()
-
-    expect(sut, toCompleteWith: nil) {
-      store.completeDeletionSuccessfully()
-      store.completeInsertionSuccessfully()
+    
+    func test_save_doesNotRequestsCacheInsertionOnDeletionError() {
+        let facts: [String] = [anyFact(), anyFact()]
+        let (sut, store) = makeSUT()
+        let deletionError = anyNSError()
+        
+        sut.save(facts) { _ in }
+        store.completeDeletion(with: deletionError)
+        
+        XCTAssertEqual(store.receivedMessages, [.deleteCachedFacts])
     }
-  }
-
-  // MARK: Helpers
-
-  private func makeSUT(
-    currentDate: @escaping () -> Date = Date.init,
-    file: StaticString = #filePath,
-    line: UInt = #line
-  ) -> (LocalCatFactLoader, CatFactStoreSpy) {
-    let store = CatFactStoreSpy()
-    let sut = LocalCatFactLoader(store: store, currentDate: currentDate)
-    trackForMemoryLeaks(store, file: file, line: line)
-    trackForMemoryLeaks(sut, file: file, line: line)
-    return (sut, store)
-  }
-
-  private func expect(
-    _ sut: LocalCatFactLoader,
-    toCompleteWith expectedError: NSError?,
-    when action: () -> Void,
-    file: StaticString = #filePath,
-    line: UInt = #line
-  ) {
-    let exp = expectation(description: "Wait for save completion")
-
-    var receivedError: Error?
-    sut.save([anyFact(), anyFact()]) { error in
-      receivedError = error
-      exp.fulfill()
+    
+    func test_save_requestsNewCacheInsertionWithTimestampOnSuccessfulDeletion() {
+        let currentDate = Date()
+        let facts: [String] = [anyFact(), anyFact()]
+        let (sut, store) = makeSUT(currentDate: { currentDate })
+        
+        sut.save(facts) { _ in }
+        store.completeDeletionSuccessfully()
+        
+        XCTAssertEqual(store.receivedMessages, [.deleteCachedFacts, .insert(facts, currentDate)])
     }
-
-    action()
-    wait(for: [exp], timeout: 1.0)
-
-    XCTAssertEqual(receivedError as? NSError, expectedError, file: file, line: line)
-  }
-
-  private class CatFactStoreSpy: CatFactStore {
-    enum ReceivedMessage: Equatable {
-      case deleteCachedFacts
-      case insert([String], Date)
+    
+    func test_save_failsOnDeletionError() {
+        let (sut, store) = makeSUT()
+        let deletionError = anyNSError()
+        
+        expect(sut, toCompleteWith: deletionError) {
+            store.completeDeletion(with: deletionError)
+        }
     }
-
-    private(set) var receivedMessages = [ReceivedMessage]()
-
-    private var deletionCompletions = [DeleteCompletion]()
-    private var insertionCompletions = [InsertCompletion]()
-
-    func deleteCachedFacts(completion: @escaping DeleteCompletion) {
-      receivedMessages.append(.deleteCachedFacts)
-      deletionCompletions.append(completion)
+    
+    func test_save_failsOnInsertionError() {
+        let (sut, store) = makeSUT()
+        let insertionError = anyNSError()
+        
+        expect(sut, toCompleteWith: insertionError) {
+            store.completeDeletionSuccessfully()
+            store.completeInsertion(with: insertionError)
+        }
     }
-
-    func completeDeletion(with error: Error, at index: Int = 0) {
-      deletionCompletions[index](error)
+    
+    func test_save_succeedsOnSuccessfulCacheInsertion() {
+        let (sut, store) = makeSUT()
+        
+        expect(sut, toCompleteWith: nil) {
+            store.completeDeletionSuccessfully()
+            store.completeInsertionSuccessfully()
+        }
     }
-
-    func completeDeletionSuccessfully(at index: Int = 0) {
-      deletionCompletions[index](nil)
+    
+    // MARK: Helpers
+    
+    private func makeSUT(
+        currentDate: @escaping () -> Date = Date.init,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> (LocalCatFactLoader, CatFactStoreSpy) {
+        let store = CatFactStoreSpy()
+        let sut = LocalCatFactLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
     }
-
-    func insert(_ facts: [String], timestamp: Date, completion: @escaping InsertCompletion) {
-      receivedMessages.append(.insert(facts, timestamp))
-      insertionCompletions.append(completion)
+    
+    private func expect(
+        _ sut: LocalCatFactLoader,
+        toCompleteWith expectedError: NSError?,
+        when action: () -> Void,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        let exp = expectation(description: "Wait for save completion")
+        
+        var receivedError: Error?
+        sut.save([anyFact(), anyFact()]) { error in
+            receivedError = error
+            exp.fulfill()
+        }
+        
+        action()
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertEqual(receivedError as? NSError, expectedError, file: file, line: line)
     }
-
-    func completeInsertion(with error: Error, at index: Int = 0) {
-      insertionCompletions[index](error)
+    
+    private class CatFactStoreSpy: CatFactStore {
+        enum ReceivedMessage: Equatable {
+            case deleteCachedFacts
+            case insert([String], Date)
+        }
+        
+        private(set) var receivedMessages = [ReceivedMessage]()
+        
+        private var deletionCompletions = [DeleteCompletion]()
+        private var insertionCompletions = [InsertCompletion]()
+        
+        func deleteCachedFacts(completion: @escaping DeleteCompletion) {
+            receivedMessages.append(.deleteCachedFacts)
+            deletionCompletions.append(completion)
+        }
+        
+        func completeDeletion(with error: Error, at index: Int = 0) {
+            deletionCompletions[index](error)
+        }
+        
+        func completeDeletionSuccessfully(at index: Int = 0) {
+            deletionCompletions[index](nil)
+        }
+        
+        func insert(_ facts: [String], timestamp: Date, completion: @escaping InsertCompletion) {
+            receivedMessages.append(.insert(facts, timestamp))
+            insertionCompletions.append(completion)
+        }
+        
+        func completeInsertion(with error: Error, at index: Int = 0) {
+            insertionCompletions[index](error)
+        }
+        
+        func completeInsertionSuccessfully(at index: Int = 0) {
+            insertionCompletions[index](nil)
+        }
     }
-
-    func completeInsertionSuccessfully(at index: Int = 0) {
-      insertionCompletions[index](nil)
+    
+    private func anyFact() -> String {
+        "any fact \(Int.random(in: 1...5))"
     }
-  }
-
-  private func anyFact() -> String {
-    "any fact \(Int.random(in: 1...5))"
-  }
-
-  private func anyNSError() -> NSError {
-    NSError(domain: "any error", code: 0)
-  }
-
+    
+    private func anyNSError() -> NSError {
+        NSError(domain: "any error", code: 0)
+    }
+    
 }
